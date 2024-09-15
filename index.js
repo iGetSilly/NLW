3455e6f871ec1602e5354810779b847d6e1daca7
GetSilly <iuoyyp@gmail.com>
2024-09-15 10:40:40 -0300

    salvando metas em JSON

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const { select, input, checkbox } = require("@inquirer/prompts")
+const fs = require("fs").promises
 
 let mensagem = "Bem vindo ao app de metas!"
-let meta = {
-    value: "Tomar 3L de água por dia",
-    checked: false,
+
+let metas
+
+const carregarMetas = async () => {
+    try {
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas = JSON.parse(dados)
+    }
+    catch(erro) {
+        metas = []
+    }
 }
 
-let metas = [meta]
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
+
+
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta:" })
@@ -78,7 +91,7 @@ const metasAbertas = async () => {
     }
 
     await select({
-        message: "Metas Abertas " + abertas.lenght(),
+        message: "Metas Abertas " + abertas.lenght,
         choices: [...abertas]
     })
 
@@ -121,8 +134,10 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
+    await carregarMetas()
     while (true) {
         mostrarMensagem()
+        await salvarMetas()
         const opcao = await select({
             message: "Menu >",
             choices: [
